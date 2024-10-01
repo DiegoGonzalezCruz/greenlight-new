47241eda77da40f78e453dbecf76886044bc33b7
Diego González Cruz <diego@go-agency.com>
2024-10-01 13:38:11 -0400

    designing buttons v2

--- packages/greenlight/themes/greenlight_theme/elements/buttons.php
+++ packages/greenlight/themes/greenlight_theme/elements/buttons.php
@@ -1,28 +1,35 @@
-<div class="flex flex-row p-5 bg-gray-500 gap-10">
-    <button class=" p-[3px] rounded w-fit bg-go-outerspace hover:bg-gradient-green-yellow-pink ">
+<div class="flex flex-wrap p-5 bg-[#C0BFBF] gap-10">
+    <button class=" p-1 rounded w-fit bg-go-outerspace hover:bg-gradient-green-yellow-pink ">
         <div class="button">
             Get in Touch
         </div>
     </button>
 
-    <button class=" p-[2px] rounded w-fit bg-white hover:bg-gradient-green-yellow-pink ">
+    <button class=" p-1 rounded w-fit bg-white hover:bg-gradient-green-yellow-pink ">
         <div class="button bg-white text-black">
             Get in Touch
         </div>
     </button>
-    <button class=" p-[2px] rounded w-fit bg-white hover:bg-gradient-green-yellow-pink ">
-        <div class="button bg-white text-black">
-            Get in Touch
-        </div>
-    </button>
-    <button class=" p-[2px] rounded w-fit border-go-outerspace border-2">
+
+    <button class="  rounded w-fit border-go-outerspace border-2">
         <div class="button bg-transparent text-black">
             Get in Touch
         </div>
     </button>
-    <button class=" p-[2px] rounded w-fit border-white border-2">
+    <button class="  rounded w-fit border-white border-2">
         <div class="button bg-transparent text-white">
             Get in Touch
         </div>
     </button>
+    <button class="  rounded w-fit ">
+        <div class="button bg-transparent text-go-outerspace hover:text-[#466F77]">
+            Get in Touch
+        </div>
+    </button>
+    <button class=" p-1 rounded w-fit bg-go-glow hover:bg-gradient-green-yellow ">
+        <div class="button bg-go-glow text-black hover:bg-gradient-green-yellow">
+            Get in Touch
+        </div>
+    </button>
+
 </div>
