613949fd368ee2e574e3798a852f921e54a1a515
Diego González Cruz <diego@go-agency.com>
2024-10-01 14:07:56 -0400

    designing more buttons and trying svg

--- packages/greenlight/themes/greenlight_theme/elements/buttons.php
+++ packages/greenlight/themes/greenlight_theme/elements/buttons.php
@@ -1,13 +1,20 @@
-<div class="flex flex-wrap p-5 bg-[#C0BFBF] gap-10">
-    <button class=" p-1 rounded w-fit bg-go-outerspace hover:bg-gradient-green-yellow-pink ">
-        <div class="button">
+<div class=" flex flex-wrap p-5 bg-[#C0BFBF] gap-10">
+    <button class="group p-1 rounded w-fit bg-go-outerspace hover:bg-gradient-green-yellow-pink ">
+        <div class="button bg-go-outerspace text-white">
             Get in Touch
+            <svg class="hidden group-hover:flex" xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14" fill="none">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M8.23932 11.74C7.94582 12.0323 7.94484 12.5072 8.23713 12.8007C8.52941 13.0942 9.00429 13.0951 9.29779 12.8029L14.5742 7.54826C14.7208 7.41137 14.8125 7.21635 14.8125 6.99992C14.8125 6.83598 14.7599 6.68433 14.6707 6.56092C14.647 6.52809 14.6205 6.49702 14.5915 6.46809L9.29774 1.19709C9.00422 0.904828 8.52935 0.905851 8.23708 1.19937C7.94482 1.4929 7.94584 1.96777 8.23937 2.26003L12.2465 6.24992H0.9375C0.523286 6.24992 0.1875 6.58571 0.1875 6.99992C0.1875 7.41414 0.523286 7.74992 0.9375 7.74992H12.246L8.23932 11.74Z" fill="white" />
+            </svg>
         </div>
     </button>
 
     <button class=" p-1 rounded w-fit bg-white hover:bg-gradient-green-yellow-pink ">
-        <div class="button bg-white text-black">
-            Get in Touch
+        <div class="button bg-white text-black ">
+            Get in Touch White
+            <svg class="invisible group-hover:visible stroke-black" xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14" fill="none">
+                <path d="M14.125 6.99988L1 6.99988" stroke="#0C111A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M8.83105 1.72855L14.1248 6.99955L8.83105 12.2714" stroke="#0C111A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
         </div>
     </button>
 
@@ -29,6 +36,16 @@
     <button class=" p-1 rounded w-fit bg-go-glow hover:bg-gradient-green-yellow ">
         <div class="button bg-go-glow text-black hover:bg-gradient-green-yellow">
             Get in Touch
+        </div>
+    </button>
+
+    <button class="group p-1 rounded w-fit bg-gradient-green-yellow hover:bg-gradient-green-yellow-pink ">
+        <div class="button !bg-none !bg-transparent">
+            Get in Touch
+            <svg class="invisible group-hover:visible" xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14" fill="none">
+                <path d="M14.125 6.99988L1 6.99988" stroke="#0C111A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M8.83105 1.72855L14.1248 6.99955L8.83105 12.2714" stroke="#0C111A" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
         </div>
     </button>
 
