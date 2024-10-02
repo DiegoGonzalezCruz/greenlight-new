caaf4dd5d40d0558b4dfc0cb48877a7ca397eebc
Diego González Cruz <diego@go-agency.com>
2024-10-02 12:32:03 -0400

    cards and buttons ready

--- packages/greenlight/themes/greenlight_theme/elements/cards.php
+++ packages/greenlight/themes/greenlight_theme/elements/cards.php
@@ -1,4 +1,3 @@
-<?php echo $view->getThemePath(); ?>
 <h2> Cards</h2>
 <div class="group flex flex-wrap p-2 bg-transparent hover:bg-gradient-green-yellow w-[412px] h-[388px] rounded-small ">
     <div class="relative w-full h-full rounded-small-internal overflow-clip ">
@@ -8,10 +7,19 @@
         </div>
     </div>
 </div>
-<div class="group flex flex-wrap p-2 bg-transparent hover:bg-gradient-green-yellow w-[573px] h-[323px] rounded-small ">
+<div class="group flex flex-wrap p-2 bg-transparent  w-[573px] h-[323px] rounded-small ">
     <div class="relative w-full h-full rounded-small-internal overflow-clip ">
         <img class="w-full h-full object-cover z-10" src="<?php echo $view->getThemePath(); ?>/images/card-work-placeholder.png" alt="Placeholder">
         <div class="w-full h-full absolute z-20 top-0 right-0 bg-black/20 group-hover:bg-black/0 text-white flex flex-col items-start justify-end p-[45px]">
+            <h6>Project Title Here</h6>
+            <h5>Client Name Here</h5>
+        </div>
+    </div>
+</div>
+<div class="group flex flex-wrap p-2 bg-transparent  w-[1170px] h-[470px] rounded-small ">
+    <div class="relative w-full h-full rounded-small-internal overflow-clip ">
+        <img class="w-full h-full object-cover z-10" src="<?php echo $view->getThemePath(); ?>/images/card-work-placeholder.png" alt="Placeholder">
+        <div class="w-full h-full absolute z-20 top-0 right-0 bg-black/20 group-hover:bg-black/0 text-white flex flex-col items-start justify-end py-[49px] px-[55px]">
             <h6>Project Title Here</h6>
             <h5>Client Name Here</h5>
         </div>
